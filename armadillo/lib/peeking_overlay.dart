55e41c0fa43b68670a27ec742f1ac8a72520a8cb
Andrew Wilson <apwilson@google.com>
2016-08-23 10:24:57 -0700

    'Rotate' peeking overlay as it closes.
    Change-Id: I7dbd17d551beefa2f99141480ec671feb1c6edc9

--- armadillo/lib/peeking_overlay.dart
+++ armadillo/lib/peeking_overlay.dart
@@ -11,6 +11,11 @@ import 'package:sysui_widgets/quadrilateral_painter.dart';
 
 const double _kStartOverlayTransitionHeight = 28.0;
 
+/// The distance the top right corner is inset when peeking.  When hiding, the
+/// top left corner will inset the same distance as the overlay becomes
+/// fully hidden.
+const double _kAngleOffsetY = 16.0;
+
 /// A bottom aligned overlay which peeks up over the bottom.
 class PeekingOverlay extends StatefulWidget {
   final double peekHeight;
@@ -63,12 +68,24 @@ class PeekingOverlayState extends BottomAlignedOverlayState<PeekingOverlay> {
     }
   }
 
+  /// Tracks how 'peeked' the overlay is taking [_kAngleOffsetY] into account.
+  /// This is used to ensure the angle of the overlay is flat as it becomes
+  /// fully 'unpeeked'. This tracks from [0.0 to 1.0] for [height]s of
+  /// [_kAngleOffsetY to config.peekHeight].
+  double get _peekProgress => math.max(
+      0.0,
+      math.min((height - _kAngleOffsetY) / (config.peekHeight - _kAngleOffsetY),
+          1.0));
+
   @override
   Widget createWidget(BuildContext context) {
     return new Stack(children: [
       new CustomPaint(
           painter: new QuadrilateralPainter(
-              topLeftInset: new Offset(0.0, 16.0 * (1.0 - openingProgress)),
+              topLeftInset:
+                  new Offset(0.0, _kAngleOffsetY * (1.0 - openingProgress)),
+              topRightInset:
+                  new Offset(0.0, _kAngleOffsetY * (1.0 - _peekProgress)),
               color: Colors.white)),
       config.child,
       new Positioned(
