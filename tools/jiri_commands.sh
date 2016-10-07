d64d75abf20a91a4ba28b600dd36d683128fdfcb
P.Y. Laligand <pylaligand@google.com>
2016-10-07 12:51:14 -0700

    Added a few extensions for jiri.
    Change-Id: Id100e16300b2f9ef55d94ad78fbaace99bee82e0

---
+++ tools/jiri_commands.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Copyright 2016 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Extensions for the jiri tool.
+#
+# Usage:
+#     jiri runp $PWD/sysui/tools/jiri_commands.sh <command>
+#
+# Available commands:
+#   - branch: list projects which have non-master branches;
+#   - master: switch all projects to their master branch;
+#   - local: list projects which are not currently on the master branch;
+#   - delete: delete a branch in all projects.
+
+command=$1
+project=`pwd`
+current_branch=`git rev-parse --abbrev-ref HEAD`
+branches=`git for-each-ref --format='%(refname:short)' refs/heads/`
+
+if [[ "$command" == "branch" ]]; then
+  if [[ "$branches" != "master" ]]; then
+    echo $project
+    echo "$branches"
+  fi
+elif [[ "$command" == "master" ]]; then
+  if [[ "$current_branch" != "master" ]]; then
+    echo $project
+    git checkout master
+  fi
+elif [[ "$command" == "local" ]]; then
+  if [[ "$current_branch" != "master" ]]; then
+    echo "$project is on $current_branch"
+  fi
+elif [[ "$command" == "delete" ]]; then
+  branch=$2
+  if [[ "$branches" == *"$branch"* ]]; then
+    echo $project
+    git branch -D $branch
+  fi
+fi
