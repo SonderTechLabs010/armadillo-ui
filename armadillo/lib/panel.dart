15f98164d919bf90a11f5657663a5eafe9bf0d38
Andrew Wilson <apwilson@google.com>
2017-02-10 16:26:03 -0800

    Raise minimum story dimensions to 360x360
    Change-Id: Iae25e6fd845f89613ee5e0a281eee1c5da11a819

--- armadillo/lib/panel.dart
+++ armadillo/lib/panel.dart
@@ -7,10 +7,10 @@ import 'dart:math' as math;
 import 'package:flutter/widgets.dart';
 
 /// The minimum dp width a panel should have.
-const double _kMinPanelWidth = 320.0;
+const double _kMinPanelWidth = 360.0;
 
 /// The minimum dp height a panel should have.
-const double _kMinPanelHeight = 320.0;
+const double _kMinPanelHeight = 360.0;
 
 /// The number of grid lines the grid should have in either direction.
 /// TODO(apwilson): This should be calculated from size rather than being a
