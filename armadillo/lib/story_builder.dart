993baa35c3a5809cd0235619d94f6a89ed910ccb
Andrew Wilson <apwilson@google.com>
2017-04-12 14:22:51 -0700

    Remove references to email module/story.
    Change-Id: I4048ceddb5c0d52a9f72006ddc10423dee025c06

--- armadillo/lib/story_builder.dart
+++ armadillo/lib/story_builder.dart
@@ -2,17 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:email_story/quarterback.dart';
-import 'package:email_session_store/email_session_store_mock.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'default_scroll_configuration.dart';
 import 'story.dart';
-
-void _createMockEmailSessionStore() {
-  kEmailSessionStoreToken ??= new StoreToken(new EmailSessionStoreMock());
-}
 
 Widget _widgetBuilder(String module, Map<String, Object> state) {
   switch (module) {
@@ -27,9 +21,6 @@ Widget _widgetBuilder(String module, Map<String, Object> state) {
               fit: BoxFit.cover,
             ),
       );
-    case 'email/quarterback':
-      _createMockEmailSessionStore();
-      return new EmailQuarterbackModule();
     default:
       return new Center(child: new Text('BAD MODULE!!!'));
   }
