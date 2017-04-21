327cd07c594bf0035b3016be62af35f160846b2f
Andrew Wilson <apwilson@google.com>
2017-04-21 14:32:38 -0700

    Implement an authentication context to show an overlay.
    Change-Id: I98a74e990c988b44224dc6316c8be3ad0cf75812

--- userpicker_device_shell/lib/main.dart
+++ userpicker_device_shell/lib/main.dart
@@ -6,12 +6,15 @@ import 'package:apps.modular.services.device/user_provider.fidl.dart';
 import 'package:apps.mozart.lib.flutter/child_view.dart';
 import 'package:apps.mozart.services.views/view_token.fidl.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:lib.fidl.dart/bindings.dart';
+import 'package:lib.widgets/model.dart';
 import 'package:lib.widgets/modular.dart';
 
+import 'authentication_context_impl.dart';
 import 'child_constraints_changer.dart';
 import 'constraints_model.dart';
 import 'user_picker.dart';
@@ -24,15 +27,31 @@ void main() {
   ConstraintsModel constraintsModel = new ConstraintsModel();
 
   UserPickerDeviceShellModel model = new UserPickerDeviceShellModel();
+  _AuthenticationOverlayModel authenticationOverlayModel =
+      new _AuthenticationOverlayModel();
+  AuthenticationContextImpl authenticationContext =
+      new AuthenticationContextImpl(
+    onStartOverlay: authenticationOverlayModel.onStartOverlay,
+    onStopOverlay: authenticationOverlayModel.onStopOverlay,
+  );
 
   DeviceShellWidget<UserPickerDeviceShellModel> deviceShellWidget =
       new DeviceShellWidget<UserPickerDeviceShellModel>(
     deviceShellModel: model,
+    authenticationContext: authenticationContext,
     child: new ChildConstraintsChanger(
       constraintsModel: constraintsModel,
-      child: new _ScreenManager(
-        key: screenManagerKey,
-        onLogout: model.refreshUsers,
+      child: new Stack(
+        children: <Widget>[
+          new _ScreenManager(
+            key: screenManagerKey,
+            onLogout: model.refreshUsers,
+          ),
+          new ScopedModel<_AuthenticationOverlayModel>(
+            model: authenticationOverlayModel,
+            child: new _AuthenticationOverlay(),
+          ),
+        ],
       ),
     ),
   );
@@ -159,4 +178,88 @@ class _ScreenManagerState extends State<_ScreenManager>
       );
     });
   }
+}
+
+class _AuthenticationOverlayModel extends Model implements TickerProvider {
+  ChildViewConnection _childViewConnection;
+
+  /// If not null, returns the handle of the current requested overlay.
+  ChildViewConnection get childViewConnection => _childViewConnection;
+
+  AnimationController _transitionAnimation;
+  CurvedAnimation _curvedTransitionAnimation;
+
+  CurvedAnimation get animation => _curvedTransitionAnimation;
+
+  /// Starts showing an overlay over all other content.
+  void onStartOverlay(InterfaceHandle<ViewOwner> overlay) {
+    _transitionAnimation = new AnimationController(
+      value: 0.0,
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _curvedTransitionAnimation = new CurvedAnimation(
+      parent: _transitionAnimation,
+      curve: Curves.fastOutSlowIn,
+      reverseCurve: Curves.fastOutSlowIn,
+    );
+    _childViewConnection = new ChildViewConnection(
+      overlay,
+      onAvailable: (ChildViewConnection connection) {
+        print('AuthenticationOverlayModel: Child view connection available!');
+        _transitionAnimation.forward();
+      },
+      onUnavailable: (ChildViewConnection connection) {
+        print('AuthenticationOverlayModel: Child view connection unavailable!');
+        _transitionAnimation.reverse();
+        // TODO(apwilson): Should not need to remove the child view
+        // connection but it causes a mozart deadlock in the compositor if you
+        // don't.
+        _childViewConnection = null;
+      },
+    );
+    notifyListeners();
+  }
+
+  /// Stops showing a previously started overlay.
+  void onStopOverlay() {
+    _transitionAnimation.reverse();
+    // TODO(apwilson): Should not need to remove the child view
+    // connection but it causes a mozart deadlock in the compositor if you
+    // don't.
+    _childViewConnection = null;
+    notifyListeners();
+  }
+
+  @override
+  Ticker createTicker(TickerCallback onTick) => new Ticker(onTick);
+}
+
+class _AuthenticationOverlay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) =>
+      new ScopedModelDescendant<_AuthenticationOverlayModel>(
+        builder: (
+          BuildContext context,
+          Widget child,
+          _AuthenticationOverlayModel model,
+        ) =>
+            model.animation == null
+                ? new Offstage()
+                : new AnimatedBuilder(
+                    animation: model.animation,
+                    builder: (BuildContext context, Widget child) =>
+                        new Opacity(
+                          opacity: model.animation.value,
+                          child: child,
+                        ),
+                    child: new FractionallySizedBox(
+                      widthFactor: 0.75,
+                      heightFactor: 0.75,
+                      child: new ChildView(
+                        connection: model.childViewConnection,
+                      ),
+                    ),
+                  ),
+      );
 }
