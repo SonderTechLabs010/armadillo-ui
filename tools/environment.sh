af1dd921842718e42529a3185a29068a21638ecb
P.Y. Laligand <pylaligand@google.com>
2017-04-28 18:28:13 -0700

    Enabling searching in folders named build/.
    Change-Id: Ib1bf68352164453f7e88b7fbad4ada1c61d318b3

--- tools/environment.sh
+++ tools/environment.sh
@@ -24,7 +24,7 @@ function push_to_gerrit() {
   git push origin HEAD:refs/for/master
 }
 
-PRUNE_ARGS="-name third_party -prune -o -name .git -prune -o -name out -prune -o -name .pub -prune -o -name .packages -prune -o -name build -prune"
+PRUNE_ARGS="-name third_party -prune -o -name .git -prune -o -name out -prune -o -name .pub -prune -o -name .packages -prune"
 GREP_COMMAND="grep -I -nH --color"
 
 function sfind() {
